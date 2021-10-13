ec2f3eb9b668567939437557555ed61866b5b400
gabecpf97 <gabrielcpf2997@gmail.com>
2021-10-13 16:06:17 -0700

    added comment to each function

--- model.js
+++ model.js
@@ -1,3 +1,8 @@
+/**
+ * Uses random function in Math to general result for either
+ * rock, paper or scissor
+ * @returns the choice of the computer in String
+ */
 function computerPlay() {
     let randomNum = Math.floor(Math.random() * 10 + 1) % 3
     switch (randomNum) {
@@ -12,10 +17,23 @@ function computerPlay() {
     }
 }
 
+/**
+ * Function that calles getResult and getStatement to general final result
+ * @param {*} player_choice     palyer's choice
+ * @param {*} computer_choice   computer's choice
+ * @returns the game's result statement in String
+ */
 function playOnce(player_choice, computer_choice){
     return getStatement(getResult(player_choice, computer_choice), player_choice, computer_choice)
 }
 
+/**
+ * Function that use the user's input for switch statement to compare with
+ * computer result to generate result of win, lose or draw the return it
+ * @param {*} player_choice     player's choice
+ * @param {*} computer_choice   computer's choice
+ * @returns the result of the game in String
+ */
 function getResult(player_choice, computer_choice) {
     player_choice = player_choice.toLowerCase()
     let result = ""
@@ -56,6 +74,13 @@ function getResult(player_choice, computer_choice) {
     return result;
 }
 
+/**
+ * Helper function that return the result statement
+ * @param {*} result    each game's result
+ * @param {*} player_choice     the player's choice
+ * @param {*} computer_choice   the computer's choice
+ * @returns string of result statement
+ */
 function getStatement(result, player_choice, computer_choice) {
     switch (result) {
         case "win":
@@ -69,6 +94,11 @@ function getStatement(result, player_choice, computer_choice) {
     }
 }
 
+/** 
+ * Function that play the game 5 time and console log the result of each game 
+ * and the final result of the game
+ * Takes no aragument
+*/
 function game() {
     let round_result = ""
     let final_result = []
@@ -82,6 +112,12 @@ function game() {
     console.log(getScore(final_result))
 }
 
+/**
+ * Helper function that get the final score of the 5 games and reutrn
+ * the final result statement of the games
+ * @param {*} final_result  array that stored 5 games' result
+ * @returns String of final result statement
+ */
 function getScore(final_result){
     let result = 0
     for (let i = 0; i < final_result.length; i++){
