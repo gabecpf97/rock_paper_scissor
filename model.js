40773a0c81f7b197ee99bc3a37ef02baa4248e70
gabecpf97 <gabrielcpf2997@gmail.com>
2021-10-13 14:46:47 -0700

    Now the computerPlay is working correctly

--- model.js
+++ model.js
@@ -3,14 +3,11 @@ let computerPlay = function() {
     switch (randomNum) {
         case 0:
             return "Rock"
-            break;
         
         case 1:
             return "Paper"
-            break;
 
         case 2:
             return "Scissor"
-            break;
     }
 }
